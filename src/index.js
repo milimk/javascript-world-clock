d2fc9485b0cd292263628480b1af303bfd4be7f8
milimk <milimkim@gmail.com>
2024-07-25 08:44:55 -0700

    JS selecting city from dropdown

--- src/index.js
+++ src/index.js
@@ -1,26 +1,80 @@
+// City Grid Display
+
 function updateTime() {
   // Vancouver
   let vanElement = document.querySelector("#vancouver");
-  let vanDateElement = vanElement.querySelector(".cityDate");
-  let vanTimeElement = vanElement.querySelector(".cityTime");
-  let vanTimeNow = moment().tz("America/Vancouver");
+  if (vanElement) {
+    let vanDateElement = vanElement.querySelector(".cityDate");
+    let vanTimeElement = vanElement.querySelector(".cityTime");
+    let vanTimeNow = moment().tz("America/Vancouver");
 
-  vanDateElement.innerHTML = vanTimeNow.format("MMMM D YYYY");
-  vanTimeElement.innerHTML = `${vanTimeNow.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  )}`;
+    vanDateElement.innerHTML = vanTimeNow.format("MMMM D YYYY");
+    vanTimeElement.innerHTML = `${vanTimeNow.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 
   // New York
   let newyorkElement = document.querySelector("#new-york");
-  let newyorkDateElement = newyorkElement.querySelector(".cityDate");
-  let newyorkTimeElement = newyorkElement.querySelector(".cityTime");
-  let newyorkTimeNow = moment().tz("America/New_York");
+  if (newyorkElement) {
+    let newyorkDateElement = newyorkElement.querySelector(".cityDate");
+    let newyorkTimeElement = newyorkElement.querySelector(".cityTime");
+    let newyorkTimeNow = moment().tz("America/New_York");
 
-  newyorkDateElement.innerHTML = newyorkTimeNow.format("MMMM D YYYY");
-  newyorkTimeElement.innerHTML = `${newyorkTimeNow.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  )}`;
+    newyorkDateElement.innerHTML = newyorkTimeNow.format("MMMM D YYYY");
+    newyorkTimeElement.innerHTML = `${newyorkTimeNow.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
+
+  // Paris
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".cityDate");
+    let parisTimeElement = parisElement.querySelector(".cityTime");
+    let parisTimeNow = moment().tz("Europe/Paris");
+
+    parisDateElement.innerHTML = parisTimeNow.format("MMMM D YYYY");
+    parisTimeElement.innerHTML = `${parisTimeNow.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
+
+  // Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".cityDate");
+    let tokyoTimeElement = tokyoElement.querySelector(".cityTime");
+    let tokyoTimeNow = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTimeNow.format("MMMM D YYYY");
+    tokyoTimeElement.innerHTML = `${tokyoTimeNow.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+// Dropdown Selecting City to update the view
+
+function updateCity(event) {
+  let selectedCityTimezone = event.target.value;
+  let selectedCityName = selectedCityTimezone.split("/")[1];
+  let selectedCityTime = moment().tz(selectedCityTimezone);
+  let selectedCityElement = document.querySelector("#gridContainer");
+
+  selectedCityElement.innerHTML = `
+  <div class="cityDetail">
+    <div class="cityName">${selectedCityName}</div>
+    <div class="cityTime">${selectedCityTime.format(
+      "hh:mm:ss [<small>]A[</small>]"
+    )}</div>
+    <div class="cityDate">${selectedCityTime.format("MMMM D YYYY")}</div>
+  </div>
+  `;
+}
+
+let selectCityElement = document.querySelector("#cities");
+selectCityElement.addEventListener("change", updateCity);
